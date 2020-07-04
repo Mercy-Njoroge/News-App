c766bd19a1044b5810f021913a571f5bbeac5e08
Mercy <aprilkasha42@gmail.com>
2020-07-04 15:08:16 +0300

    create get_sources()function,use .format method to pass the API key,get_sources_url final request for our api request,use with to send a request using urllib which takes get_sources_url and sends a request as url.use read() to read the response and store it in get_sources__data variable

--- request.py
+++ request.py
@@ -8,3 +8,22 @@ News = news.News
   api_key = app.config['NEWS_API_KEY']
   #Getting the news base url
   base_url = app.config['NEWS_API_BASE_URL']
+
+  def get_sources():
+    '''
+    This function gets json response from the news site
+    '''
+
+    get_sources_url = base_url.format(api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results = None
+
+        if get_sources_response ['sources']:
+            sources_result_list = get_sources_response['sources']
+            sources_results = process_results(sources_result_list)
+
+    return sources_results
