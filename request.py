e9636a4d777d4067372200cb7d46a0aefb0c0d4b
Mercy <aprilkasha42@gmail.com>
2020-07-04 14:48:06 +0300

    access configuration objects by calling app.config['name_of_object'],we get API key and news URL

--- request.py
+++ request.py
@@ -5,4 +5,6 @@ from .models import news
 News = news.News
 
 # Getting api key
-api_key = app.config['NEWS_API_KEY']
+  api_key = app.config['NEWS_API_KEY']
+  #Getting the news base url
+  base_url = app.config['NEWS_API_BASE_URL']
