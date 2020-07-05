3e2cc7a215c4792abc8dee250c36d59d17fef059
Mercy <aprilkasha42@gmail.com>
2020-07-05 23:41:19 +0300

    create main dir inside app and move error.py,forms.py and views.py file ro main.create init.py file

---
+++ app/main/views.py
@@ -0,0 +1,28 @@
+from flask import render_template
+from app import app
+from .request import get_sources
+from .requests import get_articles,get_article,process_article
+
+# Views
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    popular_sources = get_sources('popular')
+    print(popular_sources)
+    title = 'Home - Welcome to The best News Review Website Online'
+    return render_template('index.html', title = title, popular = popular_sources)
+
+@app.route('/news/<int:article_id>')
+def article(article_id):
+
+    '''
+    View article page function that returns the articles details page and its data
+    '''
+    article = get_article(id)
+    title = f'{article.title}'
+
+    return render_template('news.html', title = title, id = news_id)
+
