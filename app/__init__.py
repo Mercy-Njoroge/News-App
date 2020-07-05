4aceb96490e83b0b5786fb7223b5110b3a757376
Mercy <aprilkasha42@gmail.com>
2020-07-05 23:37:22 +0300

    import config_options then create bootstrap instance,create_app() function takes configuration setting key as argument,create flask app instance and import config settings.then call the init_app to complete initialization and finaly return app

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,28 @@
 from flask import Flask
-from .config import DevConfig
 from flask_bootstrap import Bootstrap
+from config import config_options
 
-# initializing application
-app = Flask(__name__,instance_relative_config = True)
+bootstrap = Bootstrap()
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+def create_app(config_name):
+    '''
+    This is a method for creating the app instance
+    '''
+    app = Flask(__name__)
 
-bootstrap = Bootstrap(app)
+    #Create the app configurations
+    app.config.from_object(config_options[config_name])
 
-from app import views
-from app import error
+    #Initialise the flask extenstions
+    bootstrap.init_app(app)
+
+    # #Registration of the blueprint
+    # from .main import main as main_blueprint
+    # app.register_blueprint(main_blueprint)
+    
+    # #Setting up config
+    # from .requests import configure_request
+    # configure_request(app)
+
+    return app
+
