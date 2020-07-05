696f10f185045165b1ad0fe26d876c6831ae7904
Mercy <aprilkasha42@gmail.com>
2020-07-05 21:43:12 +0300

    create a request and load the data and create an articles object

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
 from .request import get_sources
+from .requests import get_articles,get_article
 
 # Views
 @app.route('/')
@@ -12,7 +13,7 @@ def index():
     popular_sources = get_sources('popular')
     print(popular_sources)
     title = 'Home - Welcome to The best News Review Website Online'
-    return render_template('index.html', title = title popular = popular_sources)
+    return render_template('index.html', title = title, popular = popular_sources)
 
 @app.route('/news/<int:news_id>')
 def news(news_id):
