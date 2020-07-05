e4adb6c08c7c357830b30675ac1ba9a979f52b23
Mercy <aprilkasha42@gmail.com>
2020-07-05 23:45:38 +0300

    modify files

--- app/main/views.py
+++ app/main/views.py
@@ -1,28 +1,27 @@
-from flask import render_template
-from app import app
-from .request import get_sources
-from .requests import get_articles,get_article,process_article
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..requests import get_sources,get_articles
+from ..models import News_article,News_source 
 
-# Views
-@app.route('/')
+@main.route('/')
 def index():
-
     '''
-    View root page function that returns the index page and its data
+    Home page function returns news sources
     '''
-    popular_sources = get_sources('popular')
-    print(popular_sources)
-    title = 'Home - Welcome to The best News Review Website Online'
-    return render_template('index.html', title = title, popular = popular_sources)
 
-@app.route('/news/<int:article_id>')
-def article(article_id):
+    news_sources = get_sources()
 
+
+    title = "Welcome to news Summary a collection of all the news sources"
+    return render_template('index.html',title = title, sources = news_sources)
+
+@main.route('/articles/<articles>')
+def articles(articles):
     '''
-    View article page function that returns the articles details page and its data
+    Displays articles from a specific news source
     '''
-    article = get_article(id)
-    title = f'{article.title}'
+    
+    news_article = get_articles(articles)
 
-    return render_template('news.html', title = title, id = news_id)
-
+    title=f"You are viewing articles"
+    return render_template('articles.html', title = title,articles = news_article)
