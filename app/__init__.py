6f23d9db0fac065a71e3208ea5135ee0387986d7
Mercy <aprilkasha42@gmail.com>
2020-07-05 23:58:09 +0300

    import configure_request() function from request.py,call the function and pass the app instance

--- app/__init__.py
+++ app/__init__.py
@@ -16,13 +16,13 @@ def create_app(config_name):
     #Initialise the flask extenstions
     bootstrap.init_app(app)
 
-    # #Registration of the blueprint
-    # from .main import main as main_blueprint
-    # app.register_blueprint(main_blueprint)
+    #Registration of the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
     
-    # #Setting up config
-    # from .requests import configure_request
-    # configure_request(app)
+    #Setting up config
+    from .requests import configure_request
+    configure_request(app)
 
     return app
 
