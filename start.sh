e94f4ffcadf4d5b170dd152d42780a8052c7f460
Mercy <aprilkasha42@gmail.com>
2020-07-06 13:33:03 +0300

    export news api key and secret key,add requirement.txt file

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
- export NEWS_API_KEY=<7ad1ce120424450c8692bf39d0160962>
-export SECRET_KEY=<0758249575>
+export NEWS_API_KEY='7ad1ce120424450c8692bf39d0160962'
+export SECRET_KEY='58249575'
 
 python3.6 manage.py server
